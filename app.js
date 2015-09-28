475854983db181d5cd68c2fe00209afa7f977953
= <=>
2015-09-28 13:45:59 -0700

    add link to userBooks form

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ app.use(function(req, res, next){
 require('./controllers/index');
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log('server is listening on port ' + process.env.PORT || 3000);
+  console.log('server is listening on port ' + (process.env.PORT || 3000));
 });
