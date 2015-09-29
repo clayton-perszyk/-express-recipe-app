88ac41cb34913977c417be9a3ff8e9d791a59674
= <=>
2015-09-28 21:09:30 -0700

    add pluralize viewHelper function

--- middleware/viewHelpers.js
+++ middleware/viewHelpers.js
@@ -2,6 +2,10 @@ var db = require('../models');
 var viewHelpers = {
   capitalize: function(string){
     return string.charAt(0).toUpperCase() + string.slice(1, string.lenght);
+  },
+
+  pluralize: function(word, number){
+    return number === 1 ? word : (word + "s");
   }
 };
 
